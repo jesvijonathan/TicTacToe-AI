f8b029456726f17bfa5ff14b0f3ae30cbd1b20eb
Jesvi_Jonathan <jesvijonathan@users.noreply.github.com>
2021-01-29 03:27:31 +0530

    base done with no bugs and also added some tweaks
    Signed-off-by: Jesvi_Jonathan <jesvijonathan@users.noreply.github.com>

--- main.py
+++ main.py
@@ -120,9 +120,11 @@ copmuter = 0
 start_on = gameOn = True
 sound = True
 dif = 0
-ti = 20
-ttt = 0
+ti = 7
+ttt = 1
+target = 3
 name = ""
+dr = 0
 
 pygame.mixer.init()
 pygame.mixer.music.load("Surf_David_Renda.mp3")
@@ -187,10 +189,7 @@ def getn(p1):
         b = [100, 117, 200, 20]
         bb = (200, 127)
     else:
-        if copmuter == 1:
-            namet = "Computer"
-        else:
-            namet = "Player 2"
+        namet = "Player 2"
         b = [100, 200, 200, 20]
         bb = (200, 210)
 
@@ -205,7 +204,14 @@ def getn(p1):
                     name = name[:-1]
                 elif pygame.key.name(event.key) == "return" or pygame.key.name(event.key) == "escape":
                     f = False
-                    if name == "" or name == " ":
+                    if p1 == 2:
+                        if copmuter == 1:
+                            if name == "" or name == " ":
+                                namet = "Computer"
+                            else:
+                                copmuter = 0
+
+                    elif name == "" or name == " ":
                         name = namet
                     pygame.draw.rect(display_surface, white, b)
                     text1 = font1.render(name + " ", True, black, white)
@@ -224,10 +230,18 @@ def getn(p1):
                     pass
                 else:
                     f = False
-                    if name == "" or name == " ":
+                    if p1 == 2:
+                        if copmuter == 1:
+                            if name == "" or name == " ":
+                                namet = "Computer"
+                            else:
+                                copmuter = 0
+
+                    elif name == "" or name == " ":
                         name = namet
+
                     pygame.draw.rect(display_surface, white, b)
-                    text1 = font1.render(name + " ", True, black, white)
+                    text1 = font1.render(namet + " ", True, black, white)
                     textRect1 = text1.get_rect()
                     textRect1.center = bb
                     display_surface.blit(text1, textRect1)
@@ -253,7 +267,10 @@ def room():
     global time
     global time1
     global time2
+    global roundd
     global ttt
+    global target
+    global dr
 
     display_surface.fill(white)
 
@@ -265,6 +282,10 @@ def room():
         textRect1 = text1.get_rect()
         textRect1.center = (200, 47)
         display_surface.blit(text1, textRect1)
+
+        if dr == 1:
+            pygame.draw.rect(display_surface, color_light,
+                             [380, 35, 5, 25])
 
         text1 = font1.render("Player 1 :", True, black, white)
         textRect1 = text1.get_rect()
@@ -301,39 +322,61 @@ def room():
 
         # pygame.draw.rect(display_surface, color_light,
         #               [100, 200, 200, 28])
+        if copmuter == 1:
+            pygame.draw.rect(display_surface, white,
+                             [132+kk, 240, 160, 90])
+            text1 = font1.render("Difficulty", True, black, white)
+            textRect1 = text1.get_rect()
+            textRect1.center = (200+kk, 250)
+            display_surface.blit(text1, textRect1)
 
-        text1 = font1.render("Difficulty", True, black, white)
-        textRect1 = text1.get_rect()
-        textRect1.center = (200+kk, 250)
-        display_surface.blit(text1, textRect1)
+            if dif == 0:
+                pygame.draw.rect(display_surface, color_light,
+                                 [162+kk, 270, 80, 40])
+                text1 = font1.render("Normal", True, white, color_light)
+                textRect1 = text1.get_rect()
+                textRect1.center = (202+kk, 290)
+                display_surface.blit(text1, textRect1)
 
-        if dif == 0:
+            elif dif == 1:
+                pygame.draw.rect(display_surface, color_dark,
+                                 [162+kk, 270, 80, 40])
+                text1 = font1.render("Medium", True, white, color_dark)
+                textRect1 = text1.get_rect()
+                textRect1.center = (202+kk, 290)
+                display_surface.blit(text1, textRect1)
+            elif dif == 2:
+                pygame.draw.rect(display_surface, black,
+                                 [162+kk, 270, 80, 40])
+                text1 = font1.render("Hard", True, white, black)
+                textRect1 = text1.get_rect()
+                textRect1.center = (202+kk, 290)
+                display_surface.blit(text1, textRect1)
+
+            pygame.draw.rect(display_surface, color_dark,
+                             [140+kk, 280, 20, 20])
+            pygame.draw.rect(display_surface, color_dark,
+                             [244+kk, 280, 20, 20])
+        else:
+            pygame.draw.rect(display_surface, white,
+                             [132+kk, 240, 160, 90])
+
+            text1 = font1.render("Target", True, black, white)
+            textRect1 = text1.get_rect()
+            textRect1.center = (200+kk, 250)
+            display_surface.blit(text1, textRect1)
+
             pygame.draw.rect(display_surface, color_light,
                              [162+kk, 270, 80, 40])
-            text1 = font1.render("Normal", True, white, color_light)
+            text1 = font1.render(str(target), True, white, color_light)
             textRect1 = text1.get_rect()
             textRect1.center = (202+kk, 290)
             display_surface.blit(text1, textRect1)
 
-        elif dif == 1:
             pygame.draw.rect(display_surface, color_dark,
-                             [162+kk, 270, 80, 40])
-            text1 = font1.render("Medium", True, white, color_dark)
-            textRect1 = text1.get_rect()
-            textRect1.center = (202+kk, 290)
-            display_surface.blit(text1, textRect1)
-        elif dif == 2:
-            pygame.draw.rect(display_surface, black,
-                             [162+kk, 270, 80, 40])
-            text1 = font1.render("Hard", True, white, black)
-            textRect1 = text1.get_rect()
-            textRect1.center = (202+kk, 290)
-            display_surface.blit(text1, textRect1)
-
-        pygame.draw.rect(display_surface, color_dark,
-                         [140+kk, 280, 20, 20])
-        pygame.draw.rect(display_surface, color_dark,
-                         [244+kk, 280, 20, 20])
+                             [140+kk, 280, 20, 20])
+            pygame.draw.rect(display_surface, color_dark,
+                             [244+kk, 280, 20, 20])
 
         pygame.draw.rect(display_surface, color_light,
                          [294, 340, 64, 30])
@@ -400,22 +443,39 @@ def room():
 
                 elif 244+kk <= mouse[0] <= 244+kk+20 and 280 <= mouse[1] <= 280 + 20:
                     # [252, 280, 20, 20])
-                    if dif >= 0 and dif < 2:
-                        dif = dif+1
+                    if copmuter == 1:
+                        if dif >= 0 and dif < 2:
+                            dif = dif+1
+                    elif copmuter == 0:
+                        target += 1
+
                 elif 140+kk <= mouse[0] <= 140+kk+20 and 280 <= mouse[1] <= 280 + 20:
                     # [252, 280, 20, 20])
-                    if dif > 0 and dif <= 2:
-                        dif = dif-1
+                    if copmuter == 1:
+                        if dif > 0 and dif <= 2:
+                            dif = dif-1
+                    elif copmuter == 0:
+                        target -= 1
+                        if target <= 0:
+                            target = 1
 
                 elif 220 <= mouse[0] <= 220+20 and 280 <= mouse[1] <= 280 + 20:
                     # [252, 280, 20, 20])
-                    ti = ti-5
+                    ti = ti-2
                 elif 324 <= mouse[0] <= 324+20 and 280 <= mouse[1] <= 280 + 20:
                     # [252, 280, 20, 20])
-                    ti = ti+5
+                    ti = ti+2
+
+##
+                elif 350 <= mouse[0] <= 350+50 and 35 <= mouse[1] <= 35 + 25:
+                    dr = not dr
+                    print(dr)
+##
 
                 elif 230 <= mouse[0] <= 230+100 and 237 <= mouse[1] <= 237 + 26:
                     ttt = not ttt
+                    if ttt == 0:
+                        target = 1
                     pass
                 elif 156 <= mouse[0] <= 156+87 and 310 <= mouse[1] <= 310 + 35:
                     pygame.quit()
@@ -432,6 +492,7 @@ def room():
                     time1 = time2 = time = ti*100
                     print(time)
                     outline()
+                    roundd = 1
                     return
                 elif 45 <= mouse[0] <= 45+64 and 340 <= mouse[1] <= 340 + 30:
                     start_on = True
@@ -440,7 +501,7 @@ def room():
         pygame.display.update()
 
 
-def dr():
+def dra():
     display_surface.fill(white)
 
     pygame.draw.rect(display_surface, black,
@@ -473,8 +534,9 @@ def start_screen():
     global time2
     global ti
     global ttt
+    global target
 
-    dr()
+    dra()
 
     while start_on:
 
@@ -524,20 +586,26 @@ def start_screen():
 
                 if 156 <= mouse[0] <= 156+87 and 160 <= mouse[1] <= 160 + 35:
                     #start_on = True
+                    target = 5
+                    ti = 5
+                    time1 = time2 = time = ti*100
                     if room() == 3:
-                        dr()
+                        dra()
                     else:
                         return
 
                 elif 156 <= mouse[0] <= 156+87 and 210 <= mouse[1] <= 210 + 35:
                     start_on = False
                     reset_ne()
-                    time1 = time2 = time = 20*100
-                    ti = 20
+                    sreset()
+                    ti = 10
+                    time1 = time2 = time = ti*100
+                    target = 1
                     ttt = 0
                     outline()
 
                 elif 156 <= mouse[0] <= 156+87 and 260 <= mouse[1] <= 260 + 35:
+                    Credits()
                     pass
                 elif 156 <= mouse[0] <= 156+87 and 310 <= mouse[1] <= 310 + 35:
                     pygame.quit()
@@ -712,7 +780,7 @@ def sreset():
     turn = 0
     if ttt != 1:
         score1 = score2 = 0
-        roundd = 0
+        #roundd -=1
     else:
         pass
 
@@ -773,6 +841,7 @@ def game_logic(tile):
     global arr
     print(arr)
     global roundd
+    global dr
 
     b = None
 
@@ -799,14 +868,15 @@ def game_logic(tile):
                 pass
             else:
                 banner_draw()
-                roundd = roundd+1
+                #roundd = roundd+1
 
                 outline()
                 reset_ne()
                 global score2
                 global score1
-                score2 = score2 + 1
-                score1 = score1 + 1
+                if dr == 1:
+                    score2 += 1
+                    score1 += 1
 
     else:
         pass
@@ -815,11 +885,139 @@ def game_logic(tile):
     check_if_won()
 
 
-def count():
-    bannermain(t1="3", t3=0.5)
-    bannermain(t1="2", t3=0.5)
-    bannermain(t1="1", t3=0.5)
+def end():
+    global score1
+    global score2
+    global roundd
+    global player1
+    global player2
+    global start_on
+
+    bannermain(t1="Match Ended !", t3=1)
+    if score1 == score2:
+        bannermain(t1="Draw", t2=str(score1)+":"+str(score1) +
+                   " in "+str(roundd)+" Rounds", t3=5)
+    elif score1 > score2:
+        bannermain(t1=player1+" Wins !", t2="By "+str(score1)+":"+str(score2) +
+                   " in "+str(roundd)+" Rounds", t3=5)
+    else:
+        bannermain(t1=player2+" Wins !", t2="By "+str(score2)+":"+str(score1) +
+                   " in "+str(roundd)+" Rounds", t3=5)
+    start_on = True
+
+
+def count(sp=0.7):
+    global target
+    global ti
+    st = "Target " + str(target) + " | Time " + str(ti)+"s"
+    bannermain(t1="3", t2=st, t3=sp)
+    bannermain(t1="2", t2=st, t3=sp)
+    bannermain(t1="1", t2=st, t3=sp)
     bannermain(t1="Go !", t3=0.5)
+
+
+def Credits():
+    cred = True
+
+    while cred:
+        display_surface.fill(white)
+
+        text1 = font.render("Programed By : ", True, black, white)
+        textRect1 = text1.get_rect()
+        textRect1.center = (110, 70)
+        display_surface.blit(text1, textRect1)
+
+        text1 = font.render("Jesvi Jonathan", True, blue, white)
+        textRect1 = text1.get_rect()
+        textRect1.center = (260, 70)
+        display_surface.blit(text1, textRect1)
+
+        text1 = font.render("Version : ", True, black, white)
+        textRect1 = text1.get_rect()
+        textRect1.center = (135, 100)
+        display_surface.blit(text1, textRect1)
+
+        text1 = font.render("v0.3", True, blue, white)
+        textRect1 = text1.get_rect()
+        textRect1.center = (215, 100)
+        display_surface.blit(text1, textRect1)
+
+        text1 = font.render("Date : ", True, black, white)
+        textRect1 = text1.get_rect()
+        textRect1.center = (148, 130)
+        display_surface.blit(text1, textRect1)
+
+        text1 = font.render("28/1/2021", True, blue, white)
+        textRect1 = text1.get_rect()
+        textRect1.center = (235, 130)
+        display_surface.blit(text1, textRect1)
+        k = 30
+
+        pygame.draw.rect(display_surface, (200, 250, 205),
+                         [40, 180, 340, 120])
+
+        text1 = font.render(
+            "This was intended to be a simple game ", True, pink, (200, 250, 205))
+        textRect1 = text1.get_rect()
+        textRect1.center = (210, 170+k)
+        display_surface.blit(text1, textRect1)
+
+        text1 = font.render(
+            "powered by AI/ML to compete with users", True, pink, (200, 250, 205))
+        textRect1 = text1.get_rect()
+        textRect1.center = (210, 210+k)
+        display_surface.blit(text1, textRect1)
+
+        text1 = font.render(
+            "All built in native python (1 week)", True, pink, (200, 250, 205))
+        textRect1 = text1.get_rect()
+        textRect1.center = (210, 250+k)
+        display_surface.blit(text1, textRect1)
+
+        pygame.draw.rect(display_surface, color_light,
+                         [270, 325, 73, 24])
+        pygame.draw.rect(display_surface, color_light,
+                         [170, 325, 73, 24])
+        pygame.draw.rect(display_surface, color_light,
+                         [70, 325, 73, 24])
+
+        bt = smallfont.render('Back', True, white)
+        bt1 = smallfont.render('Website', True, white)
+        bt2 = smallfont.render('Support', True, white)
+        display_surface.blit(bt, (90, 328))
+        display_surface.blit(bt1, (180, 328))
+        display_surface.blit(bt2, (280, 328))
+
+        clock.sleep(0.01)
+
+        mouse = pygame.mouse.get_pos()
+
+        for event in pygame.event.get():
+
+            if event.type == KEYDOWN:
+
+                if event.key == K_BACKSPACE:
+                    gameOn = True
+                    display_surface.fill(white)
+                    start_on = True
+                    dra()
+                    return
+
+            elif event.type == QUIT:
+                pygame.quit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if 70 <= mouse[0] <= 70+73 and 325 <= mouse[1] <= 325 + 24:
+
+                    gameOn = True
+                    display_surface.fill(white)
+                    start_on = True
+                    dra()
+                    return
+
+        pygame.display.flip()
+        pygame.display.update()
+
+    pass
 
 
 v = 0
@@ -829,12 +1027,23 @@ while gameOn:
     if start_on == True:
         start_screen()
         bannermain(t1="Starting In", t3=1)
-        bannermain(t1="3", t3=0.7)
-        bannermain(t1="2", t3=0.7)
-        bannermain(t1="1", t3=0.7)
+        count()
         sreset()
 
     clock.sleep(0.01)
+
+    if score1 >= target and score2 >= target:
+        bannermain(t1="Draw !", t2="Score "+str(score1)+":" +
+                   str(score2)+" in "+str(roundd)+" rounds", t3=5)
+        start_on = True
+    elif score1 >= target:
+        bannermain(t1=player1+" Wins !", t2="by "+str(score1)+":" +
+                   str(score2)+" in "+str(roundd)+" rounds", t3=5)
+        start_on = True
+    elif score2 >= target:
+        bannermain(t1=player2+" Wins !", t2="by "+str(score2)+":" +
+                   str(score1)+" in "+str(roundd)+" rounds", t3=5)
+        start_on = True
 
     g1 = g = " "
     if ttt == 1:
@@ -845,11 +1054,14 @@ while gameOn:
                 time1 = time1-1
             elif time1 <= 0:
                 bannermain(t1="Time Up !", t3=1.3)
+
                 bannermain(t1=player2 + " Wins !", t2="Score +1", t3=2)
                 score2 = score2+1
                 time2 = time1 = time = ti*100
                 count()
                 sreset()
+                reset_ne()
+                roundd = 0
                 turn = 0
 
         else:
@@ -864,6 +1076,8 @@ while gameOn:
                 time1 = time2 = time = ti*100
                 count()
                 sreset()
+                reset_ne()
+                roundd = 0
                 turn = 0
     else:
         if turn % 2 == 0:
@@ -873,7 +1087,7 @@ while gameOn:
                 time1 = time1-1
             elif time1 <= 0:
                 bannermain(t1="Game Over !", t3=1.3)
-                bannermain(t1=player2 + " Wins !", t2="by " +
+                bannermain(t1=player2 + "  Wins !", t2="by " +
                            str(score1)+":"+str(score2)+" in "+str(roundd) + " rounds", t3=6)
                 start_on = True
 
@@ -884,7 +1098,7 @@ while gameOn:
                 time2 = time2-1
             elif time2 <= 0:
                 bannermain(t1="Game Over !", t3=1.3)
-                bannermain(t1=player1 + " Wins !", t2="by " +
+                bannermain(t1=player1 + "  Wins !", t2="by " +
                            str(score1)+":"+str(score2)+" in "+str(roundd) + " rounds", t3=6)
                 start_on = True
 
@@ -957,16 +1171,18 @@ while gameOn:
             if 170 <= mouse[0] <= 170+65 and 365 <= mouse[1] <= 365 + 24:
 
                 bannermain(t1="Reseted", t3=1)
-                bannermain(t1="3", t3=0.5)
-                bannermain(t1="2", t3=0.5)
-                bannermain(t1="1", t3=0.5)
-                bannermain(t1="Go !", t3=0.5)
                 time1 = time2 = time = ti*100
                 roundd = roundd
+                arr = [None, None, None,
+                       None, None, None,
+                       None, None, None]
+                count(0.5)
                 sreset()
 
             if 270 <= mouse[0] <= 270+65 and 365 <= mouse[1] <= 365 + 24:
-                pygame.quit()
+                # pygame.quit()
+                print("end")
+                end()
 
             checker_hover()
 
@@ -996,7 +1212,7 @@ while gameOn:
 
     bt = smallfont.render('Back', True, white)
     bt1 = smallfont.render('Reset', True, white)
-    bt2 = smallfont.render('Quit', True, white)
+    bt2 = smallfont.render('End', True, white)
     display_surface.blit(bt, (83, 368))
     display_surface.blit(bt1, (183, 368))
     display_surface.blit(bt2, (287, 368))
